474d36747ad6de39a46045e20448a502537d91e7
XID-POSI <103092924+XID-POSI@users.noreply.github.com>
2024-01-30 00:27:15 +0900

    :hammer: styled-component 설치 및 마킹 UI 추가

---
+++ src/component/Calendar.styled.tsx
@@ -0,0 +1,17 @@
+import { styled } from "styled-components";
+
+export const MarkingDay = styled.div`
+  position: relative;
+`;
+
+export const Dot = styled.div`
+  position: absolute;
+  top: 100%;
+  right: 0;
+  left: 0;
+  margin: 0 auto;
+  height: 4px;
+  width: 4px;
+  background-color: #63a1ff;
+  border-radius: 50%;
+`;
--- src/component/Calendar.tsx
+++ src/component/Calendar.tsx
@@ -1,14 +1,21 @@
 // type Props = {}
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Calendar as ReactCalendar } from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import moment from "moment";
+import { Dot, MarkingDay } from "./Calendar.styled";
 
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export const Calendar = () => {
   const [value, setValue] = useState<Value>(new Date());
+  const [mark, setMark] = useState<string[]>([]);
+
+  useEffect(() => {
+    const newMarks = ["2024-01-23", "2024-01-02"];
+    setMark(newMarks);
+  }, []);
 
   return (
     <>
@@ -16,6 +23,14 @@ export const Calendar = () => {
         onChange={setValue}
         value={value}
         formatDay={(_, date) => moment(date).format("D")}
+        tileContent={({ date }) => {
+          const markingDays = [];
+          if (mark.find((value) => value === moment(date).format("YYYY-MM-DD"))) {
+            markingDays.push(<Dot />);
+          }
+
+          return <MarkingDay>{markingDays}</MarkingDay>;
+        }}
       />
     </>
   );
