099e046700a7adcfc7955b64b09b2abdadb98b8f
XID-POSI <103092924+XID-POSI@users.noreply.github.com>
2024-01-29 23:59:01 +0900

    :hammer: formatDay(날짜 형식)에 moment 적용하기

--- src/component/Calendar.tsx
+++ src/component/Calendar.tsx
@@ -1,27 +1,21 @@
 // type Props = {}
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Calendar as ReactCalendar } from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import moment from "moment";
 
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export const Calendar = () => {
   const [value, setValue] = useState<Value>(new Date());
-  const [mark, setMark] = useState<string[]>([]);
-
-  useEffect(() => {
-    const newMarks = ["2024-01-23", "2024-01-02"];
-    setMark(newMarks);
-  }, []);
 
   return (
     <>
       <ReactCalendar
         onChange={setValue}
         value={value}
-        formatDay={(_, date) => date.toLocaleString("en", { day: "numeric" })}
-        tileContent={mark}
+        formatDay={(_, date) => moment(date).format("D")}
       />
     </>
   );
